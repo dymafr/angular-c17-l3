a24a335c984367647ca8c407cc2c44d0ac26e9e3
dymafr <erwan1309@hotmail.com>
2021-01-07 17:52:16 +0100

    1 file modified

--- src/app/features/panier/panier.routes.ts
+++ src/app/features/panier/panier.routes.ts
@@ -1,7 +1,5 @@
 import { Routes } from "@angular/router";
-import { PanierContainerComponent } from "./panier-container/panier-container.component";
 
 export const PANIER_ROUTES: Routes = [
-  { path: "", redirectTo: "cocktails", pathMatch: "full" },
-  { path: "panier", component: PanierContainerComponent }
+  { path: "", redirectTo: "cocktails", pathMatch: "full" }
 ];
